51c95b45235df747aa7d1a1576d81240f28e9d62
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2022-04-14 16:16:57 -0700

    test ok, clean up

--- examples/synthesis/synthesis.py
+++ examples/synthesis/synthesis.py
@@ -1,20 +1,23 @@
 import spot
-
 from crome_logic.specification.temporal import LTL
 from crome_logic.tools.io import save_to_file
 from crome_logic.typeset import Typeset
 from crome_logic.typesimple import CromeType
 from crome_logic.typesimple.subtype.base.boolean import Boolean
-from crome_synthesis.controller import Controller
 
+from crome_synthesis.controller import Controller
 from crome_synthesis.controller.synthesis import generate_controller
 
 
 def example() -> None:
-    a = LTL(formula="G(F(sens))",
-            typeset=Typeset({Boolean("sens", kind=CromeType.Kind.SENSOR)}))
-    g = LTL(formula="G(sens -> act)",
-            typeset=Typeset({Boolean("act", kind=CromeType.Kind.ACTION)}))
+    a = LTL(
+        formula="G(F(sens))",
+        typeset=Typeset({Boolean("sens", kind=CromeType.Kind.SENSOR)}),
+    )
+    g = LTL(
+        formula="G(sens -> act)",
+        typeset=Typeset({Boolean("act", kind=CromeType.Kind.ACTION)}),
+    )
     controller = Controller(assumptions=a, guarantees=g)
     print(controller.to_string("dot"))
     print(controller.to_string("lbtt"))
